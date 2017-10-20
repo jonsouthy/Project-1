bd3f07d05defe689eeffe76939b13cd4a0e700b2
jonsouthy <32474728+jonsouthy@users.noreply.github.com>
2017-10-19 22:20:09 -0500

    Delete maze.js

--- maze.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-    document.getElementById("boundary1").onmouseover = overBoundary;
-};
-
-function overBoundary() {
-    document.getElementById("boundary1").style.backgroundColor = "#ff8888";
-    document.getElementById("status").textContent = "You lose";
-}
